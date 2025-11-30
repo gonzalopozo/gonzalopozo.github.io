0c5597270220be5d516fde836232bd67d148f01a
gonzalopozo <gonzalopozo2005@gmail.com>
2025-11-30 14:36:37 +0100

    feat: added site_settings, employment_history and social_links to the portfolio CMS db scheam

--- db/schema/portfolio.ts
+++ db/schema/portfolio.ts
@@ -1,13 +1,14 @@
+import { relations } from "drizzle-orm";
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const projects = sqliteTable("projects", {
     id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
     title: text("title").notNull(),
-    desription: text("description").notNull(),
-    url: text("url").notNull(),
+    description: text("description").notNull(),
+    url: text("url"),
     repoUrl: text("repo_url").notNull(),
-    status: text("status").notNull(), // TODO: status is gonna be an enum, define the values that status can have and define a default one.
-    // order: integer().notNull(). // TODO: order should be an automatically auto-increment number but it shouldn't be a fixed value, I mean every insert should automatically set the max value in this column plus one to this field.  
+    status: text("status", { enum: ["active", "archived", "in-progress"] }).notNull(),
+    order: integer("order").notNull().default(0),
     // TODO: timestamps
 })
 
@@ -20,21 +21,40 @@ export const experiences = sqliteTable("experiences", {
     description: text("description").notNull(),
     startDate: integer("start_date", { mode: 'timestamp' }),
     endDate: integer("end_date", { mode: 'timestamp' }),
-    // order: integer().notNull(). // TODO: order should be an automatically auto-increment number but it shouldn't be a fixed value, I mean every insert should automatically set the max value in this column plus one to this field.  
-    location: integer("text"),
+    order: integer("order").notNull().default(0),
+    location: text("location"),
     // TODO: timestamps
 })
 
-export const skills= sqliteTable("skills", {
+export const skills = sqliteTable("skills", {
     id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
     name: text("name").notNull(),
     type: text("type").notNull(), // TODO: type is gonna be an enum, define the values that type can have and define a default one.
     icon: text("icon"),
-    url: text("url").notNull(),
+    url: text("url"),
     // TODO: timestamps
 })
 
 // TODO: junction tables (tablas intermedias) between projects and skills and between experiences and skills
-// TODO: site_setting table
-// TODO: employment_history table
-// TODO: social_links table
+
+export const siteSettings = sqliteTable("site_settings", {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    isEmployed: integer("is_employed", { mode: 'boolean' }),
+    resumeUrl: text("resume_url").notNull(),
+    statusMessage: text("status_message"),
+    // TODO: timestamps -> updated_at
+})
+
+export const employmentHistory = sqliteTable("employment_history", {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    isEmployed: integer("is_employed", { mode: 'boolean' }),
+    // TODO: timestamps -> changed_at
+})
+
+export const socialLinks = sqliteTable("social_links", {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    name: text("name").notNull(),
+    url: text("url").notNull(),
+    icon: text("icon"),
+    order: integer("order").notNull().default(0),
+})
