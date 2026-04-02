3891d2e3ef7ce01f5e6d1a92daf0bc57ead2c53f
gonzalopozo <gonzalopozo2005@gmail.com>
2026-04-02 18:55:01 +0200

    feat: approach to test invalid IPs v6

--- lib/og-image.ts
+++ lib/og-image.ts
@@ -1,8 +1,14 @@
 // import "server-only"
 import { URL } from "url";
 import { resolve4, resolve6 } from "dns/promises";
+import { updateExperience } from "./actions/experiences";
 
-function isPrivateIpV4(ip: string): boolean {
+/**
+ * Retorna true/false dependiendo de si la dirección IP es invalida o valida respectivamente.
+ * @param ip Dirección IP recibida
+ * @returns Retorna true si es una IP invalida y false si es valida.
+ */
+function isInvalidIpV4(ip: string): boolean {
     const octects = ip.split(".").map(Number);
     if (octects.length !== 4 || octects.some((p) => isNaN(p))) return true;
 
@@ -22,11 +28,75 @@ function isPrivateIpV4(ip: string): boolean {
         ((a === 198) && (b >= 18 && b <= 19)) ||
         ((a === 198) && (b === 51) && (c === 100)) ||
         ((a === 203) && (b === 0) && (c === 113)) ||
-        (a >= 224 && a <= 255)
+        (a >= 224)
     ) return true;
-    
+
     return false;
 }
+
+function isInvalidIpV6(ip: string): boolean {
+    if (ip.split("::").length > 2) return true;
+
+    let ipFilled;
+
+    if (ip.includes("::")) {
+        const ipSplited = ip.split("::");
+    
+        const leftIpPart = ipSplited[0].split(":");
+        const rightIpPart = ipSplited[1].split(":");
+    
+        const leftGroupsCount = ((leftIpPart.length === 1) && (leftIpPart[0] === "")) ? 0 : leftIpPart.length;
+        const rightGroupsCount = ((rightIpPart.length === 1) && (rightIpPart[0] === "")) ? 0 : rightIpPart.length;
+    
+        const zeroGroupsToAdd = 8 - leftGroupsCount - rightGroupsCount;
+    
+        let zerosFiller = "";
+    
+        if (leftGroupsCount !== 0) zerosFiller += ":";
+        for (let i = 0; i < zeroGroupsToAdd; i++) {
+            zerosFiller += "0:"
+        }
+        if (rightGroupsCount === 0) zerosFiller = zerosFiller.slice(0, -1)
+    
+        ipFilled = ip.replaceAll("::", zerosFiller);
+    } else ipFilled = ip;
+
+    const groups = ipFilled.split(":").map(octect => parseInt(octect, 16));
+    if (groups.length !== 8 || groups.some((p) => isNaN(p))) return true;
+
+    const [g0, g1, g2, g3, g4, g5, g6, g7] = groups; 
+
+    if (
+        ((g0 === 0) && (g1 === 0) && (g2 === 0) && (g3 === 0) && (g4 === 0) && (g5 === 0) && (g6 === 0) && (g7 >= 0 && g7 <= 1)) ||
+        ((g0 === 100) && (g1 === 65435) && (g2 === 1)) ||
+        ((g0 === 256) && (g1 === 0) && (g2 === 0) && (g3 === 0)) || 
+        ((g0 === 8193) && (g1 === 0)) || 
+        ((g0 === 8193) && (g1 === 2)) || 
+        ((g0 === 8193) && (g1 >= 16 && g1 <= 31)) || 
+        ((g0 === 8193) && (g1 === 3512)) || 
+        (g0 === 8194) || 
+        (g0 >= 64512 && g0 <= 65023) || 
+        (g0 >= 65152 && g0 <= 65215) || 
+        (g0 >= 65280)  
+    ) return true;
+
+    if (
+        ((g0 === 0) && (g1 === 0) && (g2 === 0) && (g3 === 0) && (g4 === 0) && (g5 === 65535)) ||
+        ((g0 === 100) && (g1 === 65435) && (g2 === 0) && (g3 === 0) && (g4 === 0) && (g5 === 0))
+    ) {
+        const octet1 = g6 >> 8;
+        const octet2 = g6 & 255;
+        const octet3 = g7 >> 8;
+        const octet4 = g7 & 255;
+
+        const extracedIpV4 = `${octet1}.${octet2}.${octet3}.${octet4}`
+
+        if (isInvalidIpV4(extracedIpV4)) return true;
+    }
+
+    return false;
+}
+
 
 /**
  * Guarda la imagen en el storage (Vercel Blob)
@@ -49,13 +119,23 @@ export async function saveImageInVercelBlob(url: string): Promise<string | null>
 
     console.log("El protocolo es https")
 
-    const test = await resolve4(webUrl.hostname);
-    // resolve6
+    const revolvedIpsV4  = await resolve4(webUrl.hostname);
 
-    console.log("dns-resolve4 test:", test)
+    for (const ip of revolvedIpsV4) {
+        if (isInvalidIpV4(ip)) return null;
+    }
 
+    console.log("Todas las IPs V4 son validas");
+
+    const revolvedIpsV6  = await resolve6(webUrl.hostname);
+
+    for (const ip of revolvedIpsV6) {
+        if (isInvalidIpV6(ip)) return null;
+    }
+
+    console.log("Todas las IPs V6 son validas");
 
     return null;
 }
 
-// void saveImageInVercelBlob("https://nbamon.gonzalopozo.dev/");
+void saveImageInVercelBlob("https://nbamon.gonzalopozo.dev/");
