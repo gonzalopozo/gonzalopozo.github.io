5885e0e27a3c2c9a3f6c4f3f331128cc7528932a
gonzalopozo <gonzalopozo2005@gmail.com>
2026-04-05 13:03:03 +0200

    feat: call the utility from createProject and updateProject

--- lib/actions/projects.ts
+++ lib/actions/projects.ts
@@ -38,13 +38,31 @@ export async function createProject(formData: FormData) {
     console.log("projectIdData", projectIdData);
     console.log("projectSkillsData", projectSkillsData);
 
+    const newProjectId = projectIdData[0].insertedId;
+
     const projectSkillsInsert: ProjectSkill[] = projectSkillsData.map(skill => {
-        return { projectId: projectIdData[0].insertedId, skillId: skill }
+        return { projectId: newProjectId, skillId: skill }
     })
 
     console.log("projectSkillsInsert", projectSkillsInsert);
 
     await db.insert(projectSkills).values(projectSkillsInsert);
+
+    if (url) {
+        try {
+            const uploadedImageUrl = await saveImageInVercelBlob(url, newProjectId);
+
+            if (uploadedImageUrl) {
+                try {
+                    await db.update(projects).set({ ogImageUrl: uploadedImageUrl }).where(eq(projects.id, newProjectId));
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
     redirect("/dashboard/projects")
 }
@@ -56,18 +74,22 @@ export async function updateProject(id: number, formData: FormData) {
     const repoUrl = formData.get('repoUrl') ? formData.get('repoUrl') as string : undefined;
     const status = formData.get('status') as ProjectStatus;
 
-    const projectIdData = await db.update(projects).set({
+    const oldUrls = await db.select({ url: projects.url, ogImageUrl: projects.ogImageUrl }).from(projects).where(eq(projects.id, id));
+
+    const oldOpenGraphImageUrl = oldUrls[0].ogImageUrl;
+
+    await db.update(projects).set({
         title,
         description,
         url,
         repoUrl,
         status,
-    }).where(eq(projects.id, id)).returning({ insertedId: projects.id });
+    }).where(eq(projects.id, id));
 
     const projectSkillsData = formData.getAll('skillIds').map(Number);
 
     const projectSkillsToReview: ProjectSkill[] = projectSkillsData.map(skill => {
-        return { projectId: projectIdData[0].insertedId, skillId: skill }
+        return { projectId: id, skillId: skill }
     })
 
     const formerProjectSkills: ProjectSkill[] = await db.query.projectSkills.findMany({
@@ -100,6 +122,36 @@ export async function updateProject(id: number, formData: FormData) {
         }
     }
 
+        if (url && (url !== oldUrls[0].url)) {
+            try {
+                const uploadedImageUrl = await saveImageInVercelBlob(url, id);
+    
+                if (uploadedImageUrl) {
+                    try {
+                        await db.update(projects).set({ ogImageUrl: uploadedImageUrl }).where(eq(projects.id, id));
+
+                        if (oldOpenGraphImageUrl) {
+                            await deleteOldImageInVercelBlob(oldOpenGraphImageUrl);
+                        }
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+
+            } catch (e) {
+                console.log(e);
+            }
+        } else if (!url) {
+            if (oldOpenGraphImageUrl) {
+                await deleteOldImageInVercelBlob(oldOpenGraphImageUrl);
+
+                try {
+                    await db.update(projects).set({ ogImageUrl: null }).where(eq(projects.id, id));
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        }
 
     redirect("/dashboard/projects")
 }
