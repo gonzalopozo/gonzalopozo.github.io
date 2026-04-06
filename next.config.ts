58eb10afb3cc6bd38729bc80aa459e86ac82d4f9
gonzalopozo <gonzalopozo2005@gmail.com>
2026-04-06 09:36:51 +0200

    perf: every icons package added to optimizePackageImports experimental feature in next config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true,
     turbopackFileSystemCacheForBuild: true,
+    optimizePackageImports: [
+      "lucide-react",
+      "react-icons",
+      "@radix-ui/react-icons",
+    ],
   },
   images: {
     remotePatterns: [new URL(`${process.env.BLOB_PUBLIC_HOSTNAME}/**`)]
